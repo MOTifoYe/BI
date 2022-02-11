fcc61ed255ca9b509ad140ce4d9db040dd8e8a04
Тимофей Перминов <goaway0137@outlook.com>
2022-02-11 17:01:30 +0700

    Тёмная тема.

--- WpfTestResources/MainWindow.xaml.cs
+++ WpfTestResources/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace WpfTest2
 {
@@ -20,14 +12,36 @@ namespace WpfTest2
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        List<ResourceDictionary> resources = new List<ResourceDictionary>();
         public MainWindow()
         {
             InitializeComponent();
+            
+            resources.Add( Application.LoadComponent(new Uri("ToggleThemeButton.xaml", UriKind.Relative)) as ResourceDictionary);
+            resources.Add( Application.LoadComponent(new Uri("Light.xaml", UriKind.Relative)) as ResourceDictionary);
+            resources.Add( Application.LoadComponent(new Uri("Dark.xaml", UriKind.Relative)) as ResourceDictionary);
+
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resources[0]);
+            Application.Current.Resources.MergedDictionaries.Add(resources[1]);
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void toggleTheme_Checked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Remove(resources[1]);
+            Application.Current.Resources.MergedDictionaries.Add(resources[2]);   
+        }
+
+        private void toggleTheme_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Remove(resources[2]);
+            Application.Current.Resources.MergedDictionaries.Add(resources[1]);
         }
     }
 }
