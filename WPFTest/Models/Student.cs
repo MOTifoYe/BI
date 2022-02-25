fc0b6b347100796f86d3a4fa3cc6377cabf8d1ae
MOTifoYe VS <goaway0137@outlook.com>
2022-02-26 00:55:43 +0700

    New file project.

---
+++ WPFTest/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WPFTest
+{
+    class Student
+    {
+        private string _name = "DefaultName";
+        public string Name
+        {
+            get => _name;
+            internal set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new ArgumentException("Укажите имя", value);
+                _name = value;
+            }
+        }
+        private int _mark;
+        public string Mark
+        {
+            get { return _mark.ToString(); }
+            internal set
+            {
+                bool v = int.TryParse(value, out int inMark);
+                if (!v)
+                    throw new ArgumentException("Укажите корректное значение оценки", value);
+                if (inMark < 2 || inMark > 5)
+                    throw new ArgumentException("Значение оценки должно быть от 2 до 5", value);
+                _mark = inMark;
+            }
+        }
+    }
+}
---
+++ WPFTest/PasswordWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Логика взаимодействия для PasswordWindow.xaml
+    /// </summary>
+    public partial class PasswordWindow : MetroWindow
+    {
+        public PasswordWindow()
+        {
+            InitializeComponent();
+        }
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        public string Password
+        {
+            get { return passwordBox.Text; }
+        }
+    }
+}
